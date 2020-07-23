786b700357d866313f7d9c42d9a392837bd03ac3
shalinie22 <60962646+shalinie22@users.noreply.github.com>
2020-07-24 00:52:03 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,187 +1,156 @@
-import React , {Component} from "react";
 
-class Left extends Component{
+import React from "react";
 
-    constructor (props){
-        super(props)
-        this.state={
-            fname:"",
-            lname:"",
-            email:"",
-            password:"",
-            repass:"",
-            marital:"",
-            food:"",
-            countries : [],
-			states : [],
-            selectedCountry : '--Choose Country--',
-            selectedState : '--Choose State--',
-            details:[]
-		};
-		this.changeCountry = this.changeCountry.bind(this);
-		this.changeState = this.changeState.bind(this);
+const Left=()=>{
+    const [fname,setfname]=React.useState("");
+    const [lname,setlname]=React.useState("");
+    const [email,setemail]=React.useState("");
+    const [password,setPassword]=React.useState("");
+    const [repass,setrepass]=React.useState("");
+    const [state, setstate] = React.useState([]);
+    const [marital,setmarital]=React.useState("");
+    const [food,setfood]=React.useState("");
+    const [country, setcountry] = React.useState("");
+   const [selectstate, setselectstate] = React.useState("");
+const[arr,setarr]=React.useState([])
 
-    }
-    componentDidMount() {
-		this.setState({
-			countries : [
-				{ name: 'India', states: [ "TamilNadu" ,"Kerala","Delhi"] },
-				{ name: 'USA', states: [ "NewYork","California","Texas" ] },
-					]
-		});
-	}
-
-
-appendChild = () => {
-    let { details } = this.state;
-    details.push(details.length); 
-    this.setState({details});
-  };
-changeCountry(event) {
-    this.setState({selectedCountry: event.target.value});
-    this.setState({states : this.state.countries.find(cntry => cntry.name === event.target.value).states});
-}
-
-changeState(event) {
-    this.setState({selectedState: event.target.value});
-   }
-
-    handlefname=(e)=>{this.setState(
-
-        {fname:e.target.value,
-            
-       },
+ const selection = [
+    {
+      name: "INDIA",
+      states: ["tamilnadu", "kerala", "Delhi"],
+    },
+    {
+      name: "USA",
+      states: ["New York", "California", "Texas"],
+    },
+  ];
+  const countrysetting=(e)=> {
+    setcountry(e.target.value);
+    setstate(selection.filter((d, i) => d.name == e.target.value)[0].states);
+  }
+    // const handlefname=(e)=>{setfname(e.target.value)}
+    // const handlelname=(e)=>{setlname(e.target.value)}
+    // const handleemail=(e)=>{setemail(e.target.value)}
+    // const handlepassword=(e)=>{setPassword(e.target.value)}
+    // const handlerepass=(e)=>{setrepass(e.target.value)}
+    // const handlemarital=(e)=>{setmarital(e.target.value)}
+    // const handlefood=(e)=>{setfood(e.target.value)}
    
-        )
-}
-handlelname=(e)=>{this.setState(
-    {
-    lname:e.target.value}
-    )
-}
-handleemail=(e)=>{this.setState(
-    {email:e.target.value
-    }
-    )
-}
-handlepassword=(e)=>{this.setState(
-    {
-    password:e.target.value}
-    )
-}
-handlerepass=(e)=>{this.setState(
-    {
-    repass:e.target.value}
-    )
-}
-handlemarital=(e)=>{this.setState(
-    {
-    marital:e.target.value}
-    )
-}
-handlefood=(e)=>{this.setState(
-    {
-    food:e.target.value}
-    ) 
-}
-handlecountry=(e)=>{this.setState(
-    {
-    country:e.target.value}
     
-    )  
-}
+    const changeState=(event)=> {setselectstate( event.target.value)}
 
-handleform=(e)=>{
-   alert(`${this.state.fname},${this.state.lname},${this.state.email},${this.state.food},${this.state.country}`)
-}
+   function submit() {
+       console.log(fname, lname, email, marital, food, country, selectstate)
+       console.log("hai")
+    if (password === repass) {
+      return setarr(
+        arr.concat([
+          { fname, lname, email, marital, food, country, selectstate },
+        ]),
+console.log("array",arr),
+        setfname(""),
+        setlname(""),
+        setemail(""),
+        setrepass(""),
+        setPassword(""),
+        setmarital(""),
+        setfood(""),
+        setcountry("")
+      );
+    alert("okkk")
+    } else {
+      alert("confirm password Password is incorrect");
+    }
+  }
 
-
-
-
-    render(){
-    return (
-        
-        <>
+    return(
        
+        
+         <>
         <div style={{width:"50%",backgroundColor:"whitesmoke",float:"left",height:"100%"}}>
            <center>
                <h1>Register</h1>
-               <form onSubmit={this.handleform}>
+               <form >
                <table>
+               <thead>
+               </thead>
+               <tbody>
                <tr>
-               <td><h5>FirstName</h5></td><td> <input type="text" value={this.state.fname}
-        onChange={this.handlefname} /></td></tr>
-               <tr><td><h5>LastName </h5></td><td><input type="text" value={this.state.lname}
-        onChange={this.handlelname}/></td></tr>
-               <tr><td><h5>Email</h5></td><td> <input type="email" value={this.state.email}
-        onChange={this.handleemail} /></td></tr>
-                <tr><td><h5>Password</h5></td><td> <input type="password" value={this.state.password}
-        onChange={this.handlepassword} /></td></tr>
-               <tr><td><h5>Retype Password   </h5></td><td><input type="password" value={this.state.repass}
-        onChange={this.handlerepass} /></td></tr>
-               <tr><td><h5>Maritial Status   </h5></td><td><h6><input type="radio"/>Single<input type="radio"/>Married</h6></td></tr> 
-               
-               <tr><td> <h5>Choice of food </h5></td>
-               <td><select value={this.state.food} onChange={this.handlefood}>
-                   <option value="Sandwitch">Sandwitch</option>
-                   <option value="Maggi">Maggi</option>
-                   <option value="Pizza">Pizza</option>
-                   <option value="Burger">Burger</option>
-                   </select></td></tr>
+               <td><h5>FirstName</h5></td><td> <input type="text" value={fname}
+        onChange={(e)=>setfname(e.target.value)} /></td></tr>
+               <tr><td><h5>LastName </h5></td><td><input type="text" value={lname}
+        onChange={(e)=>setlname(e.target.value)}/></td></tr>
+               <tr><td><h5>Email</h5></td><td> <input type="email" value={email}
+        onChange={(e)=>setemail(e.target.value)} /></td></tr>
+                <tr><td><h5>Password</h5></td><td> <input type="password" value={password}
+        onChange={(e)=>setPassword(e.target.value)} /></td></tr>
+               <tr><td><h5>Retype Password   </h5></td><td><input type="password" value={repass}
+        onChange={(e)=>setrepass(e.target.value)} /></td></tr>
+               <tr><td><h5>Maritial Status   </h5></td><td><h6><input type="radio" onChange={(e) => setmarital(e.currentTarget.value)}/>Single<input type="radio" onChange={(e) => setmarital(e.currentTarget.value)}/>Married</h6></td></tr> 
+                <tr><td> <h5>Choice of food </h5></td>
+                <td><select value={food} onChange={(e)=>setfood(e.target.value)}>
+                    <option value="Sandwitch">Sandwitch</option>
+                    <option value="Maggi">Maggi</option>
+                    <option value="Pizza">Pizza</option>
+                    <option value="Burger">Burger</option>
+                    </select></td></tr>
 
                   
-                   <tr><td><h5>Country</h5></td>
-                   <td><select placeholder="Country" value={this.state.selectedCountry} onChange={this.changeCountry}>
-						<option>--Choose Country--</option>
-						{this.state.countries.map((e, key) => {
-							return <option key={key}>{e.name}</option>;
-						})}
-					</select></td>
-                   </tr>
-                   <tr><td><h5>State</h5></td>
-                   <td><select placeholder="State" value={this.state.selectedState} onChange={this.changeState}>
-						<option>--Choose State--</option>
-						{this.state.states.map((e, key) => {
-							return <option key={key}>{e}</option>;
-						})}
-					</select></td>
-                   </tr>
-               </table>
-               <button type="submit" >Submit</button>
-               </form>
+                    <tr><td><h5>Country</h5></td>
+                    <td><select  value={country} onChange={(e)=>countrysetting(e)}>
+ 						<option>--Choose Country--</option>
+              <option>INDIA</option>
+              <option>USA</option>
+ 					</select></td>
+                    </tr>
+                    <tr><td><h5>State</h5></td>
+                    <td><select value={selectstate} onChange={(e)=>changeState(e)}>
+ 						<option>--Choose State--</option>
+ 						{state.map((e, key) => {
+ 							return <option key={key}>{e}</option>;
+ 						})}
+ 					</select></td>
+                    </tr>
+                    
+              </tbody>
+                 </table>
+                </form>
+              <button  onClick={() => submit()} >Submit</button>
            </center>
         </div>
-        
+        { arr==""?<p>NO DATA</p>:
         <div style={{width:"50%",float:"right",height:"100%"}}>
-        <table>
-        <thead>
-        <tr>
-            <th style={{width:"150px"}} >FirstName</th>
-            <th style={{width:"150px"}}>LastName</th>
-            <th style={{width:"150px"}}>Email</th>
-            <th style={{width:"150px"}}>food</th>
-            <th style={{width:"150px"}}>marital status</th>
-            <th style={{width:"150px"}}>country</th>
-            <th style={{width:"150px"}}>state</th>
-        </tr>
-        </thead>
-        <tbody>
-         {this.state.details.map((a, index) => {
-            return(<tr key={a.id}>
-              <td>{a.this.state.fname}</td>
-              <td>{a.this.state.lname}</td>
-              <td>{a.this.state.email}</td>
-              <td>{a.this.state.food}</td>
-              <td>{a.this.state.marital}</td>
-              <td>{a.this.state.selectedCountry}</td>
-              <td>{a.this.state.selectedState}</td>
-            </tr>
-          )})} 
-          </tbody>
-        </table>
-       </div> 
-        </>
+          <table>
+            <thead>
+              <tr>
+                <th>First name</th>
+                <th>Last name:</th>
+                <th>E-mail</th>
+                <th>Status</th>
+                <th>Choice of food</th>
+                <th>Country</th>
+                <th>state</th>
+              </tr>
+            </thead>
+            <tbody>
+              {arr.map((data, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{data.fname}</td>
+                    <td>{data.lname}</td>
+                    <td>{data.email}</td>
+                    <td>{data.marital}</td>
+                    <td>{data.food}</td>
+                    <td>{data.country}</td>
+                    <td>{data.selectstate}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>}
+        </> 
+        
     )
-    }
 }
-export default Left;
+export default Left
